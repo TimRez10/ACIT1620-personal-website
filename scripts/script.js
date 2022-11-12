9354015c89e72161ebe67ee6a9819aa92b3938ea
Timur R <timurreziapov2@gmail.com>
2022-11-12 12:39:48 -0800

    adds a loop that checks if inputed code matches existing code and an if statement that adds the code if it does not exist

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,25 @@
-courseList = [
+let courseList = [
     { code: 'ACIT 1620' ,  name: 'Scripting for IT'},
     { code: 'ACIT 1515' ,  name: 'Web Fundamental Technologies'},
     { code: 'ACIT 1630' ,  name: 'Database systems'},
 ];
 
 
-
-let class_code;
+let input_code;
 do {
-    class_code = prompt("Enter a 4-digit number");
-    console.log((Number(class_code)).length);
-} while((isNaN(class_code)) || !((class_code.length == 4))); 
+    input_code = prompt("Enter a 4-digit number");
+} while((isNaN(input_code)) || !((input_code.length == 4))); 
+
+isClass = false
+for (let lookup of courseList) {
+    let course_num = lookup.code.split(" ")[1];
+    if (course_num === input_code) {
+        console.log(`Yes I am taking the course:  ${lookup.code} - ${lookup.name}`)
+        isClass = true
+    }
+};
+
+if (isClass === false) {
+    courseList.push({ code: input_code ,  name: null})
+    console.log(`Successfully added ${input_code} code. New courseList array is:`, courseList)
+}
